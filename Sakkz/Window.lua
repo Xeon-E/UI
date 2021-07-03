ec03226097662e56b06a015700da35ba713d5f8c
Xeon-E <83479315+Xeon-E@users.noreply.github.com>
2021-07-03 19:45:58 -0400

    Update Window.lua

--- Sakkz/Window.lua
+++ Sakkz/Window.lua
@@ -5,13 +5,13 @@ local UserInputService = game:GetService("UserInputService")
 function Window:New()
     local Window = {}
 
-    Window.Object = Sakkz.require("Sakkz\\Base Objects\\Window.lua")()
+    Window.Object = loadstring(game:HttpGet("https://raw.githubusercontent.com/Sakkzz/UI/master/Sakkz/Base%20Objects/Window.lua", true))()
     Window.Object.Parent = game.CoreGui
 
     Window.Tiles = 1
 
     function Window:Tile(icon, label)
-        local Tile = Sakkz.require("Sakkz\\Tile.lua"):New(icon, label, Window)
+        local Tile = loadstring(game:HttpGet("https://raw.githubusercontent.com/Sakkzz/UI/master/Sakkz/Tile.lua", true))():New(icon, label, Window)
     
         return Tile
     end
