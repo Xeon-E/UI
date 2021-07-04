dcb5e87f5bc46dc3d5d2b1887cd417c81dcab056
Xeon-E <83479315+Xeon-E@users.noreply.github.com>
2021-07-03 20:03:08 -0400

    Update Groupbox.lua

--- Sakkz/Groupbox.lua
+++ Sakkz/Groupbox.lua
@@ -4,7 +4,7 @@ function Groupbox:New(position, size, label, Tile)
     local Groupbox = {
         NextPosition = 10
     }
-    Groupbox.Object = Sakkz.require("Sakkz\\Base Objects\\Groupbox.lua")()
+    Groupbox.Object = loadstring(game:HttpGet("https://raw.githubusercontent.com/Xeon-E/UI/master/Sakkz/Base Objects/Groupbox.lua"))()()
 
     Groupbox.Object.Parent = Tile.Content
     Groupbox.Object.Title.Text = label
@@ -12,25 +12,25 @@ function Groupbox:New(position, size, label, Tile)
     Groupbox.Object.Position = position
 
     function Groupbox:Checkbox(label, callback)
-        local Checkbox = Sakkz.require("Sakkz\\Checkbox.lua"):New(label, callback, Groupbox)
+        local Checkbox = loadstring(game:HttpGet("https://raw.githubusercontent.com/Xeon-E/UI/master/Sakkz/Checkbox.lua"))():New(label, callback, Groupbox)
 
         return Checkbox
     end
 
     function Groupbox:Button(label, callback)
-        local Checkbox = Sakkz.require("Sakkz\\Button.lua"):New(label, callback, Groupbox)
+        local Checkbox = loadstring(game:HttpGet("https://raw.githubusercontent.com/Xeon-E/UI/master/Sakkz/Button.lua"))():New(label, callback, Groupbox)
 
         return Checkbox
     end
 
     function Groupbox:Slider(label, options, callback)
-        local Checkbox = Sakkz.require("Sakkz\\Slider.lua"):New(label, options, callback, Groupbox)
+        local Checkbox = loadstring(game:HttpGet("https://raw.githubusercontent.com/Xeon-E/UI/master/Sakkz/Slider.lua"))():New(label, options, callback, Groupbox)
 
         return Checkbox
     end
 
     function Groupbox:Dropdown(label, options, callback)
-        local Dropdown = Sakkz.require("Sakkz\\Dropdown.lua"):New(label, options, callback, Groupbox)
+        local Dropdown = loadstring(game:HttpGet("https://raw.githubusercontent.com/Xeon-E/UI/master/Sakkz/Dropdown.lua"))():New(label, options, callback, Groupbox)
 
         return Dropdown
     end
