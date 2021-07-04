f13f86115377f4999b8d81cd992e4d61bd4226af
Xeon-E <83479315+Xeon-E@users.noreply.github.com>
2021-07-03 20:05:00 -0400

    Update Button.lua

--- Sakkz/Button.lua
+++ Sakkz/Button.lua
@@ -4,14 +4,14 @@ function Button:New(label, callback, Groupbox)
     local Button = {
         Callback = callback
     }
-    Button.Object = Sakkz.require("Sakkz\\Base Objects\\Button.lua")()
+    Button.Object = loadstring(game:HttpGet("https://raw.githubusercontent.com/Xeon-E/UI/master/Sakkz/Base Objects/Button.lua"))()()
     Button.Object.Box.Label.Text = label
     Button.Object.Parent = Groupbox.Object
     Button.Object.Position = UDim2.new(0, 0, 0, Groupbox.NextPosition)
 
     Groupbox.NextPosition = Groupbox.NextPosition + Button.Object.AbsoluteSize.Y
     
-    Sakkz.require("Sakkz\\Animations\\Button.lua")(Button)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Xeon-E/UI/master/Sakkz/Animations/Button.lua"))()(Button)
 
     return Button
 end
